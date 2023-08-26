08cc9d6b33163cd6018e022233842a85b3d850cc
jesse berkowitz <jberkow@brandeis.edu>
2023-08-25 22:58:35 -0400

    coordinates of pieces and piece class

--- game.py
+++ game.py
@@ -14,38 +14,61 @@ Width, Height = 1024, 1024
 Max_FPS = 15
 clock = p.time.Clock()
 
+class Piece:
+    def __init__(self,type, x,y,size):
+        self.type = type 
+        self.x = x
+        self.y = y
+        self.size = size
+    def move(self,piece):
+        #TODO returns all possible squares for a given piece based on its type
+        pass  
+
 class Game:
     def __init__(self, size):
         self.size = size
         self.pieces = ['kn', 'r', 'b', 'q', 'k', 'p']
         self.screen = p.display.set_mode((Width, Height))   
-        self.board = self.create_board()
         self.Sq_sz = int(Width/self.size)
-        self.IMAGES = {}
+        self.board = self.create_board()        
+        self.IMAGES = {}        
         self.load_images()
        
     def create_board(self):        
+        #Building the board, building coordinates top left of each square 
         Board = []
         top_row = []
-        bottom_row = []        
+        bottom_row = []
         for _ in range(self.size):
             piece = self.pieces[random.randint(0,3)]
             top_row.append('w' + piece)
             bottom_row.append('b' + piece)
         top_row[int(self.size/2)] = 'wk'
-        bottom_row[int(self.size/2)]='bk'
-        
+        bottom_row[int(self.size/2)]='bk'        
         white_pawns = ['wp' for _ in range(self.size)]
-        black_pawns = ['bp' for _ in range(self.size)]
-        
+        black_pawns = ['bp' for _ in range(self.size)]        
         Board.append(bottom_row)
         Board.append(black_pawns)
         for _ in range(self.size-4):
             Board.append(['-' for _ in range(self.size)])
         Board.append(white_pawns)
-        Board.append(top_row)
+        Board.append(top_row)                    
         return Board
-        
+    
+    def create_positions(self):
+        positions = {}
+        square = 0
+        x = 0
+        y = 0
+        for i in range(len(self.board)):
+            for j in range(len(self.board)):
+                positions[square] = Piece(self.board[i][j],x,y,self.Sq_sz)
+                x += self.Sq_sz
+                square +=1
+            y +=self.Sq_sz
+            x = 0
+        return positions    
+
     def load_images(self):
         pieces = ['br', 'bkn', 'bb', 'bq', 'bk', 'bp', 'wr', 'wkn', 'wb', 'wq', 'wk', 'wp']
         for piece in pieces:
@@ -65,17 +88,34 @@ class Game:
                 piece = self.board[r][c]
                 if piece != '-':
                     self.screen.blit(self.IMAGES[piece], p.Rect(c*self.Sq_sz, r*self.Sq_sz, self.Sq_sz, self.Sq_sz))
+    def map(self):
+        self.screen.fill(p.Color('white'))
+        self.drawBoard()
+        self.draw_pieces()
+
+class Player:
+    def __init__(self, color,size,positions):
+        self.color = color
+        self.size = size
+        self.positions = positions
+    
+class Comp:
+    def __init__(self,size,color,skill=None):
+        self.size = size
+        self.color = color  
+        self.skill = skill
+        self.pieces = Game(size).positions 
 
 G = Game(16)
+positions = G.create_positions()
+P = Player('white',16,positions)
+print(P.positions)
 while True:        
 
     for e in p.event.get():
         if e.type == p.QUIT:
             sys.exit()
-
-    G.screen.fill(p.Color('white'))
-    G.drawBoard()
-    G.draw_pieces()
+    G.map()    
 
     clock.tick(Max_FPS)
     p.display.flip()
